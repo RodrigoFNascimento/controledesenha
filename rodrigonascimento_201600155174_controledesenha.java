07951f60a7049859d76a208a2b5add55951e7aaf
RodrigoFNascimento <rodrigofn0@hotmail.com>
2019-09-08 15:44:29 -0300

    Add input processing

--- rodrigonascimento_201600155174_controledesenha.java
+++ rodrigonascimento_201600155174_controledesenha.java
@@ -22,11 +22,11 @@ class Person {
 
     String  name;
     int     age;
-    Organ   organ;
+    String   organ;
     int     number;
     int     priority;
 
-    public Person(String name, int age, Organ organ, int number, int priority) {
+    public Person(String name, int age, String organ, int number, int priority) {
         this.name       = name;
         this.age        = age;
         this.organ      = organ;
@@ -36,6 +36,27 @@ class Person {
 }
 
 public class rodrigonascimento_201600155174_controledesenha {
+
+    public static Organ makeOrgan(String line) {
+        int whitespace = line.indexOf(" ");
+        String organName = line.substring(0, whitespace);
+        int cashiers = Integer.parseInt(line.substring(whitespace + 1));
+        return new Organ(organName, cashiers);
+    }
+
+    public static Person makePerson(String line, int number) {
+        int firstPole = line.indexOf("|");
+        int secondPole = line.indexOf("|", firstPole + 1);
+        String organ = line.substring(0, firstPole);
+        String name = line.substring(firstPole + 1, secondPole);
+        int age = Integer.parseInt(line.substring(secondPole + 1));
+        int priority;
+        if (age >= 60)
+            priority = 1;
+        else
+            priority = 0;
+        return new Person(name, age, organ, number, priority);
+    }
 
     /**
      * Writes content to file.
@@ -61,6 +82,20 @@ public class rodrigonascimento_201600155174_controledesenha {
         try (FileInputStream inputStream = new FileInputStream(args[0])) {
 
             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            int numOfOrgans = Integer.parseInt(reader.readLine());
+            Organ[] organs = new Organ[numOfOrgans];
+
+            for(int i = 0; i < numOfOrgans; i++) {
+                organs[i] = makeOrgan(reader.readLine());
+            }
+
+            int numOfPeople = Integer.parseInt(reader.readLine());
+            Person[] people = new Person[numOfPeople];
+
+            for(int i = 0; i < numOfPeople; i++) {
+                people[i] = makePerson(reader.readLine(), i);
+            }
 
         } catch (Exception ex) {
             ex.printStackTrace();
